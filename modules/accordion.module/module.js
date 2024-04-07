43f12d4f8a8f3a1489991546e567712d866a99df
Matt Windle <matthew.windle26@gmail.com>
2024-04-07 15:56:46 +0100

    Bug Fix: Multiple lines of content wasn't hiding

--- modules/accordion.module/module.js
+++ modules/accordion.module/module.js
@@ -2,7 +2,7 @@ const accordions = document.querySelectorAll(".accordion-wrapper .accordion");
 console.log(accordions);
 accordions.forEach((accordion) => {
   const title = accordion.querySelector("p.accordion__title");
-  const text = accordion.querySelector("p.accordion__text");
+  const text = accordion.querySelector(".accordion__text");
   title.addEventListener("click", () => {
     text.classList.toggle("show");
     accordion.classList.toggle("expanded");
